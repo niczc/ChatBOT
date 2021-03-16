bcef50faa76006be2065f75a80657465edd3a1d0
niczc <50963474+niczc@users.noreply.github.com>
2021-03-16 10:24:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def welcome(update, context):
 
 
 def lista_replay(update, context):
-    message = '⚠️  Ordem Semanal dos Replays:  ⚠️\n 📌 Quarta-Feira 19:00 (17/03): Gustavo Teles \n  📌 Sexta-Feira 19:00 (19/03): Renan Galvan \n 📌 Sábado 13:00 (20/03): Guilherme Turibio'
+    message = '⚠️  Ordem Semanal dos Replays:  ⚠️\n 📌 Quarta-Feira 20:00 (17/03): Gustavo Teles \n  📌 Sexta-Feira 19:00 (19/03): Renan Galvan \n 📌 Sábado 13:00 (20/03): Guilherme Turibio'
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
     
 def feedback(update, context):
